29201849fecc2172d5e296bc4069fa668889cc52
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2016-02-16 16:40:45 -0500

    Fix to share buttons text in description

--- res/application/views/template/javascript.php
+++ res/application/views/template/javascript.php
@@ -11,8 +11,8 @@ if(typeof wabtn4fg==="undefined")   {wabtn4fg=1;h=document.head||document.getEle
  $(document).ready(function(){
     $('.share').ShareLink({
         title: '<?php echo $facebook['titulo'] ?>',
-        text: '<?php echo $facebook['mensaje'] ?>',
-        image: "<?php echo $facebook['url_image_facebook'] ?>",
+        text: '<?php echo json_encode($facebook['mensaje']) ?>',
+        image: '<?php echo $facebook['url_image_facebook']?>',
         url: location.href, // link on shared page
         // class_prefix: 's_', // optional class prefix for share elements (buttons or links or everything), default: 's_'
         width: 700, // optional popup initial width
