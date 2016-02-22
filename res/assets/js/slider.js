054852c25edf0054292d7def3e57797300b483c8
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2016-02-22 17:52:03 -0500

    Las TAGS estan implementadas

--- res/assets/js/slider.js
+++ res/assets/js/slider.js
@@ -2,5 +2,9 @@ $('.multiple-items').slick({
   arrows: false,
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  centerPadding: '15px',
+  customPaging: '15px',
+
 });
