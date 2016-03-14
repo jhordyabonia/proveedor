26f670f83e7ab579c5408ef226da19d3aad2da41
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2016-03-14 10:00:31 -0500

    Modificado el top menu con nuevos iconos

--- res/application/views/index/top_menu.php
+++ res/application/views/index/top_menu.php
@@ -10,60 +10,37 @@
 	    			</div>
 	    		</div>
 	    		<div class="col-xs-12 col-md-5 bor_botoom">
-	    			<div class="row">
-						<ul class="list-inline contenedor_gral">
-							<li class="active">
-								<ul class="list-inline contenedor_top">
-									<?php if($id_usuario):?>
-										<a class="enlace_top" href="<?=base_url()?>logueo/logout">
-											<li><span class="ingresar_ico glyphicon glyphicon-user"></span></li>
-											<li>Salir</li>
-										</a>
-									<?php else: ?>
-										<li><span class="ingresar_ico glyphicon glyphicon-user"></span></li>
-										<!-- <i class="fa fa-sign-in"></i> -->
-										<li><a id="launch_login" class="enlace_top"  href="JavaScript:login(this,'tablero_usuario');"
-											class="enlace_top enlace_registro cursor-mano  hidden-xs hidden-sm visible-md visible-lg" style="color:#111;">Ingresar</a></li>
-									<?php endif; ?>
-								</ul>
-							</li>
-							<li>
-								<ul class="list-inline contenedor_top hidden-xs hidden-sm visible-md visible-lg">
-									<li>
-
-									<!-- <img src="<?php echo base_url()?>assets/img/index/registro.png"  style="width: 23px;margin-left: -12px;"> -->
-									<span class="glyphicon glyphicon-pencil"></span>
-									</li>
-									<?php if($id_usuario):?>
-										<li><a class="enlace_top  hidden-xs hidden-sm visible-md visible-lg" href="<?=base_url()?>tablero_usuario">Tablero de usuario</a></li>										
-									<?php else: ?>
-										<li>
-											<a id="launch_registro" data-toggle="modal" href="JavaScript:launch(this);"
-											class="enlace_top enlace_registro cursor-mano  hidden-xs hidden-sm visible-md visible-lg" style="color:#111;">
-												Registro Gratis
-											</a>
-										</li>								
-									<?php endif; ?>
-								</ul>
-							</li>
-							<!--
-							<li>
-								<ul class="list-inline" style="border-right: 2px solid #D9D9D9;height: 23px;">
-									<li><img src="<?php echo base_url()?>assets/img/index/contacto.png"  style="width: 29px;"></li>
-									<li><p style="color:black; font-size:12px; margin-right: 10px;">Contactos</p></li>
-								</ul>
-							</li>
-							<li>
-								<ul class="list-inline" style="border-right: 2px solid #D9D9D9;height: 23px;">
-									<li></li>
-									<li><p style="color:black; font-size:12px; margin-right: 22px;">Ayuda</p></li>
-								</ul>
-							</li>
-						-->
-						</ul>
+				    <ul class="list-inline pull-right">
+				        <li class="active">
+				            <?php if($id_usuario):?>
+				                <a class="enlace_top" href="<?=base_url()?>logueo/logout">
+				                    <span class="fa fa-sign-out"></span>
+				                    Salir
+				                </a>
+				            <?php else: ?>
+				                <a id="launch_login" class="enlace_top"  href="JavaScript:login(this,'tablero_usuario');"
+				                class="enlace_top enlace_registro cursor-mano  hidden-xs hidden-sm visible-md visible-lg" style="color:#111;">
+				                    <span class="ingresar_ico glyphicon glyphicon-user"></span>
+				                    Ingresar
+				                </a>
+				            <?php endif; ?>
+				        </li>
+				        <li>
+				            <?php if($id_usuario):?>
+				                <a class="enlace_top  hidden-xs hidden-sm visible-md visible-lg" href="<?=base_url()?>tablero_usuario">
+				                    <i class="fa fa-desktop"></i>
+				                    Tablero de usuario
+				                </a>                                      
+				            <?php else: ?>
+				                <a id="launch_registro" data-toggle="modal" href="JavaScript:launch(this);"
+				                            class="enlace_top enlace_registro cursor-mano  hidden-xs hidden-sm visible-md visible-lg" style="color:#111;">
+				                    <span class="fa fa-check"></span>
+				                    Registro Gratis
+				                </a>
+				            <?php endif; ?>
+				        </li>                               
+				    </ul>
 				</div>
-	    		</div>
-	    		
 	    	</div>
 	    </div>
 <script type="text/javascript">              
