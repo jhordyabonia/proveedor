7ac23d63aa41efa65578afdbafcc787e5a8c3e24
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2016-03-15 10:39:00 -0500

    Slider de videos funcionando

--- res/assets/js/slider.js
+++ res/assets/js/slider.js
@@ -14,3 +14,19 @@ $('.slider_producto').slick({
   slidesToScroll: 1,
   swipeToSlide: true,
 });
+
+autoPlayYouTubeModal();
+
+//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+function autoPlayYouTubeModal() {
+    var trigger = $("body").find('[data-toggle="modal"]');
+    trigger.click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-idvideo"),
+            videoSRCauto = "http://www.youtube.com/embed/" + videoSRC + "?autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRCauto);
+        });
+    });
+}
