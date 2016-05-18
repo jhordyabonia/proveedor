678bc2482a668361c8eb1eedcbf507b6ce976792
jhordy abonia <jhordyabonia@bitbucket.org>
2016-05-17 20:09:16 -0500

    archivo temporal con estilos css

---
+++ res/application/views/tablero_usuario/new/style_tmp.php
@@ -0,0 +1,122 @@
+<style>
+	.validacion_error
+	{
+		color:#dd4b39;
+	}
+	.validacion_success
+	{
+		color:#00a65a;
+	}
+    .msj_err_xz 
+    {
+		color:#dd4b39;
+        font-size:13px;
+        font-family:Arial;
+    }
+    .validacion_success_input
+     {color:#00a65a; border: 1px solid #00a65a;}
+                        
+    .validacion_error_input
+    {color:#dd4b39; border: 1px solid #dd4b39;}
+    input
+    { 
+      border: 1px solid #d2d6de;
+      margin-bottom: 3%!important;
+      /*margin:2% 2% 2% 2%;*/
+    }
+    textarea{        
+      margin-bottom: 3%!important;
+    }
+    select{        
+      margin-bottom: 3%!important;
+    }
+    .imgxx
+    {
+    max-width: 100%;
+    max-height: 100%;
+    }
+    .content_imxx
+    {        
+        width: 100%;
+    height: 299px;
+    padding: 2% 5% 10% 2%;
+    }
+    .span12
+{
+	width: 100%;
+	height: 100%;
+}
+.text-left {
+    text-align: left;
+    font-size: 12px;
+}
+.cell
+{
+	padding: 5px;
+    font-size: 16px;
+    height: 10px;
+    width: 2%;
+    border-right: 1px solid #F5F5F5;
+    border-top: 1px solid rgb(185, 179, 179);
+    color: #333333;
+    cursor: pointer;
+}
+.row1
+{
+    background-color: white;
+}
+.new
+{
+    font-weight: bold;
+}
+.badge
+{
+	background-color: #FF7F27!important;
+}
+.conten-general-cata 
+{
+    width: 80%!important;
+}
+.elipse 
+{
+    white-space: nowrap; 
+    max-width: 10em; 
+    overflow: hidden;
+    text-overflow: ellipsis; 
+    /*border: 1px solid #000000;*/
+}
+a.logo{
+    background-color: #FF5722!important;
+}
+.skin-proveedor .content-wrapper, .skin-proveedor .main-footer {
+    border-left: 1px solid #d2d6de;
+    background-color: #f9fafc;
+}
+.bg-green, .callout.callout-success, .alert-success, .label-success, .modal-success .modal-body {
+    background-color: #0073B7 !important;
+}
+.callout.callout-warning, .alert-warning, .label-warning, .modal-warning .modal-body {
+    background-color: #0073B7 !important;
+}
+.sidebar {
+    padding-bottom: 10px;
+    background-color: white;
+}
+.content-img-circle {
+    margin: 0;
+    padding: 20% 0% 0% 0%;
+    border-radius: 50%;
+    height: 80px;
+    max-width: 80px;
+    overflow: hidden;
+    align-items: center;
+    align-content: center;
+    background-color: white;
+    border: solid 1px #F39C12;
+}
+    .imgxx1
+    {
+    max-width: 80%;
+    max-height: 80%;
+    }
+</style>
